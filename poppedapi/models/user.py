61c556ebd3e43b6b1c3a14c83dcc56bb026d2793
Kristen Chandler <krismchandler@gmail.com>
2023-01-07 11:52:06 -0600

    updated models to match updated erd

--- poppedapi/models/user.py
+++ poppedapi/models/user.py
@@ -6,9 +6,7 @@ class User(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     username = models.CharField(max_length=50)
-    user_genres = models.CharField(max_length=100)
     image_url = models.CharField(max_length=1000)
-    watchlist = models.CharField(max_length=100)
     uid = models.CharField(max_length=100)
 
     def __str__(self):
