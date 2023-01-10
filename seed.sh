cf0b7a628fc0ad02c8eaa98c3301d190e0ce5168
Kristen Chandler <krismchandler@gmail.com>
2023-01-09 20:33:11 -0600

    setup and seeded flick_cast_crew sample data

--- seed.sh
+++ seed.sh
@@ -8,11 +8,11 @@ python3 manage.py loaddata flicks
 python3 manage.py loaddata genres
 python3 manage.py loaddata moods
 python3 manage.py loaddata users
-# python3 manage.py loaddata flick_cast_crews
-# python3 manage.py loaddata flick_genres
-# python3 manage.py loaddata flick_moods
-# python3 manage.py loaddata flick_recommended_bys
-# python3 manage.py loaddata friend_requests
-# python3 manage.py loaddata friends
-# python3 manage.py loaddata user_flicks
-# python3 manage.py loaddata user_genres
+python3 manage.py loaddata flick_cast_crews
+python3 manage.py loaddata flick_genres
+python3 manage.py loaddata flick_moods
+python3 manage.py loaddata flick_recommended_bys
+python3 manage.py loaddata friend_requests
+python3 manage.py loaddata friends
+python3 manage.py loaddata user_flicks
+python3 manage.py loaddata user_genres
