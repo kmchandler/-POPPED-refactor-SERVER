f2b8f27f5ea5cf13271a4604e97f110e5385c2de
Kristen Chandler <krismchandler@gmail.com>
2023-01-13 12:38:33 -0600

    linked view sets to init

--- poppedapi/views/__init__.py
+++ poppedapi/views/__init__.py
@@ -1,2 +1,9 @@
 from .auth import check_user, register_user
 from .user import UserView
+from .flick_cast_crew import FlickCastCrewView
+from .flick_genre import FlickGenreView
+from .flick_recommended_by import FlickRecommendedByView
+from .flick import FlickView
+from .genre import GenreView
+from .mood import MoodView
+from .user_genre import UserGenreView
